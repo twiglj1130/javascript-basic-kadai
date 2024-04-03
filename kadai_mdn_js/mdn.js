deeb9e92dd7a2113fa643e508d315fab0c8f8f02
twiglj1130 <twig_lj_1130@yahoo.co.jp>
2024-04-03 19:34:39 +0900

    今日の情報を引き出す

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,16 @@
+// todayの宣言し、全部取得その中からそれぞれの値を引き出す
+let today = new Date();
+
+// 年だけ取得
+let year = today.getFullYear();
+// console.log(year);
+
+// 月だけ取得
+let month = today.getMonth();
+// console.log(month);
+
+// 日にちだけ取得
+let day = today.getDate();
+// console.log(day);
+
+console.log(year + '年' + month + '月' + day + '日')
